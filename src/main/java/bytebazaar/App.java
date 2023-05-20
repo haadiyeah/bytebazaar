d913b67d759acbe38e0c79b595bad560be48b73a
mahad002 <i211239@nu.edu.pk>
2023-05-20 12:34:02 +0500

    hello

--- src/main/java/bytebazaar/App.java
+++ src/main/java/bytebazaar/App.java
@@ -30,9 +30,10 @@ public class App extends Application {
         return fxmlLoader.load();
     }
 
+
     public static void main(String[] args) {
-        System.out.println("JavaFX Version: " + System.getProperty("javafx.version"));
-        System.out.println("JavaFX Runtime Version: " + System.getProperty("javafx.runtime.version"));
+        // System.out.println("JavaFX Version: " + System.getProperty("javafx.version"));
+        // System.out.println("JavaFX Runtime Version: " + System.getProperty("javafx.runtime.version"));
         launch();
     }
 }
