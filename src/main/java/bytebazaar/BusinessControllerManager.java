4cfc4dfd6e357ab98721bf0db1d3a3ea032e7a19
haadiyeah <pazocado@gmail.com>
2023-06-05 18:27:51 +0500

    Improved the singleton design of BusinessCtrlrMgr

--- src/main/java/bytebazaar/BusinessControllerManager.java
+++ src/main/java/bytebazaar/BusinessControllerManager.java
@@ -3,33 +3,47 @@ package bytebazaar;
 //A class that contains singleton instances of different business controllers.
 
 public class BusinessControllerManager {
-    private static adminController adminController=null;
-    private static BuyerController buyerController=null;
-    private static SellerController sellerController=null;
+    private static BusinessControllerManager instance=null;
+    private adminController adminController=null;
+    private BuyerController buyerController=null;
+    private SellerController sellerController=null;
 
     private BusinessControllerManager(){
+        adminController = new adminController();
+        buyerController=new BuyerController();
+        sellerController=new SellerController();
     }
 
     public static adminController getAdminControllerInst(){
-        if(adminController==null) 
-            adminController = new adminController();
-
-        return adminController;
+        if (instance == null) {
+            synchronized (BusinessControllerManager.class) {
+                if (instance == null) {
+                    instance = new BusinessControllerManager();
+                }
+            }
+        }
+        return instance.adminController;
     }
 
     public static BuyerController getBuyerControllerInst() {
-        if(buyerController==null) {
-            buyerController=new BuyerController();
+        if (instance == null) {
+            synchronized (BusinessControllerManager.class) {
+                if (instance == null) {
+                    instance = new BusinessControllerManager();
+                }
+            }
         }
-        
-        return buyerController;
+        return instance.buyerController;
     }
 
     public static SellerController getSellerControllerInst() {
-        if(sellerController==null) {
-            sellerController=new SellerController();
+        if (instance == null) {
+            synchronized (BusinessControllerManager.class) {
+                if (instance == null) {
+                    instance = new BusinessControllerManager();
+                }
+            }
         }
-        return sellerController;
+        return instance.sellerController;
     }
-    
 }
